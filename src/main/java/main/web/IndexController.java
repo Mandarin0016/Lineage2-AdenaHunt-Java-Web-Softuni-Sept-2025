b16703081e76007d699adcbd423610ca07fa1969
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-10-16 17:39:13 +0300

    initial commit

--- src/main/java/main/web/IndexController.java
+++ src/main/java/main/web/IndexController.java
@@ -1,10 +1,7 @@
 package main.web;
 
-import main.web.dto.LoginRequest;
-import main.web.dto.RegisterRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -13,23 +10,5 @@ public class IndexController {
     public String getIndexPage() {
 
         return "index";
-    }
-
-    @GetMapping("/register")
-    public ModelAndView getRegisterPage() {
-
-        ModelAndView modelAndView = new ModelAndView("register");
-        modelAndView.addObject("registerRequest", new RegisterRequest());
-
-        return modelAndView;
-    }
-
-    @GetMapping("/login")
-    public ModelAndView getLoginPage() {
-
-        ModelAndView modelAndView = new ModelAndView("login");
-        modelAndView.addObject("loginRequest", new LoginRequest());
-
-        return modelAndView;
     }
 }
